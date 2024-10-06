b6c67ccb11adf2b1c529a300aea5bce8568287ca
Sameera Madushan <sameera.xyz.me@gmail.com>
2024-10-07 01:37:00 +0530

    Added district code for each districts in english, sinhala and tamil

--- database/migrations/2024_10_06_143846_create_districts_table.php
+++ database/migrations/2024_10_06_143846_create_districts_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('districts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('province_id')->constrained('provinces')->onDelete('cascade');
+            $table->string('district_code_en'); // District code in English
+            $table->string('district_code_si'); // District code in Sinhala
+            $table->string('district_code_ta'); // District code in Tamil
             $table->string('name_en'); // District name in English
             $table->string('name_si'); // District name in Sinhala
             $table->string('name_ta'); // District name in Tamil
