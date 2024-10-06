1c80b9523015e910e4a87b481f1ce1c9bf227e02
Sameera Madushan <sameera.xyz.me@gmail.com>
2024-10-06 21:17:06 +0530

    Created Postal codes modal, migration and seeders.

---
+++ database/migrations/2024_10_06_151446_create_postal_codes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('postal_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('post_office_name'); // Post office name
+            $table->string('postal_code'); // Postal code
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('postal_codes');
+    }
+};
