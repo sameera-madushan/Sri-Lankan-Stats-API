e18842eeb83bfbb41e228dbd6bd35bd77d031cb2
Sameera Madushan <sameera.xyz.me@gmail.com>
2024-10-07 18:35:24 +0530

    /provinces and /districts endpoints added

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\DistrictController;
+use App\Http\Controllers\ProvinceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::apiResource('/provinces', ProvinceController::class)->only(['index', 'show']);
+    Route::apiResource('/districts', DistrictController::class)->only(['index', 'show']);
+});
