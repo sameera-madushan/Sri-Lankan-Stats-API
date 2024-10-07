7a4a9f991e40bbefe303b8a00567d0e826bd93c2
Sameera Madushan <sameera.xyz.me@gmail.com>
2024-10-07 18:35:45 +0530

    Added realtionship between provinces

--- app/Models/District.php
+++ app/Models/District.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Province;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class District extends Model
 {
     use HasFactory;
+
+    public function province()
+    {
+        return $this->belongsTo(Province::class, 'province_id');
+    }
 }
