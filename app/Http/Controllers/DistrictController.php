06893c50dd67ea10949699932c92bcca3a52657d
Sameera Madushan <sameera.xyz.me@gmail.com>
2024-10-07 22:14:42 +0530

    CHnage controller function names

--- app/Http/Controllers/DistrictController.php
+++ app/Http/Controllers/DistrictController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\DistrictResource;
 use App\Models\District;
 use Illuminate\Http\Request;
+use App\Http\Resources\CityResource;
+use App\Http\Resources\DistrictResource;
 
 class DistrictController extends Controller
 {
-    public function index()
+    public function getAllDistricts()
     {
         $provinces = District::all();
         return DistrictResource::collection($provinces);
+    }
+
+    public function getAllCitiesInDistrict(District $district)
+    {
+        $cities = $district->cities;
+        return CityResource::collection($cities);
     }
 }
--- app/Http/Controllers/ProvinceController.php
+++ app/Http/Controllers/ProvinceController.php
@@ -2,19 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
 use App\Models\Province;
 use Illuminate\Http\Request;
+use App\Http\Resources\CityResource;
+use App\Http\Resources\DistrictResource;
 use App\Http\Resources\ProvinceResource;
 
 class ProvinceController extends Controller
 {
-    public function index()
+    public function getAllProvinces()
     {
         $provinces = Province::all();
         return ProvinceResource::collection($provinces);
     }
 
-    public function show(){
+    public function getAllDistrictsInProvince(Province $province){
 
+        $districts = $province->district;
+        return DistrictResource::collection($districts);
+    }
+
+    public function getAllCitiesInProvince(Province $province){
+
+        $districts = $province->district()->with('cities')->get();
+        $cities = $districts->pluck('cities')->flatten();
+        return CityResource::collection($cities);
     }
 }
