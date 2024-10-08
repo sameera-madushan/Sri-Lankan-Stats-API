0d7001c160842b8def30a524f647770a1aabd99d
Sameera Madushan <sameera.xyz.me@gmail.com>
2024-10-08 14:34:47 +0530

    add OpenAPI annotations for province endpoints

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,27 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Sri Lankan Stats API",
+ *      description="API that provides detailed information about Sri Lanka, including postal codes, districts, provinces, etc...",
+ *      @OA\Contact(
+ *          email="sameera.xyz.me@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ *
+ * @OA\Server(
+ *      url="http://127.0.0.1:8000/api/v1",
+ *      description="Local API Server"
+ * )
+ *
+ */
+
 abstract class Controller
 {
     //
--- app/Http/Controllers/DistrictController.php
+++ app/Http/Controllers/DistrictController.php
@@ -9,12 +9,43 @@ use App\Http\Resources\DistrictResource;
 
 class DistrictController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/districts",
+     *     summary="Get list of districts",
+     *     tags={"Districts"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *     ),
+     * )
+    */
     public function getAllDistricts()
     {
         $provinces = District::all();
         return DistrictResource::collection($provinces);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/districts/get-cities/{district}",
+     *     summary="Get all cities in a district",
+     *     tags={"Districts"},
+     *     @OA\Parameter(
+     *         name="district",
+     *         in="path",
+     *         required=true,
+     *         description="The ID of the district",
+     *         @OA\Schema(
+     *             type="integer"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful response",
+     *     )
+     * )
+     */
     public function getAllCitiesInDistrict(District $district)
     {
         $cities = $district->cities;
