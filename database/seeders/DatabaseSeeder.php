89e461bbbe35c227b3191ccf85314f94a8dda0a7
Sameera Madushan <sameera.xyz.me@gmail.com>
2024-10-09 19:56:48 +0530

    Added DistrictMapSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             PostalCodeSeeder::class,
             PollingDistrictSeeder::class,
             PollingDivisionSeeder::class,
+            DistrictMapSeeder::class,
         ]);
     }
 }
